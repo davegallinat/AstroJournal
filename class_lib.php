3f0138ecadd964b36912e5bfd2d770fee140aa57
plaidmelon <dave@plaidmelon.com>
2017-03-14 01:46:09 -0400

    shortcode done

--- class_lib.php
+++ class_lib.php
@@ -62,26 +62,31 @@ class pm_customAdminColumns
 }
 
 
-/**************************************************************
-* This creates the AstroJournal shortcode - [aj_observations] *
-***************************************************************/
+/****************************************************************
+* This creates the AstroJournal shortcode - [aj_observations]   *
+* Also adds a helper metabox to the edit screen showing post id *
+*****************************************************************/
 class aj_shortcode
 {
 	public function __construct()
 	{
 		add_shortcode('aj_observations', array($this, 'build_aj_shortcode'));
+		add_action('add_meta_boxes', array($this, 'add_aj_shortcode_metabox'));
 	}
 	
 	public function build_aj_shortcode($atts)
 	{
-		
+		//global $post;
 		$new_atts = shortcode_atts(array(
-			'post_type'   => 'astrojournal',
-			'post_status' => 'publish',
-			'display'     => 'list'
-			
+			'post_type'      => 'astrojournal',
+			'p'              => '',			// post ID
+			'posts_per_page' => '-1',
+			'show'           => 'list',		// just titles or with excerpts, or full content
+			'orderby'        => 'post_date',
+			'order'          => 'DESC'
 		), $atts);
 		
+		$postID = $new_atts['p'];
 		
 		// global namespace \WP_Query
 		$query = new \WP_Query($new_atts);
@@ -89,19 +94,65 @@ class aj_shortcode
 		
 		if ($query -> have_posts())
 		{
-			$string .= '<ul>';
 			while ($query -> have_posts())
 			{
 				$query -> the_post();
-				$string .= '<li><a href="'.get_the_permalink().'">' . get_the_title() . '</a></li>';
+				$post_class = join(' ', get_post_class());
+				
+				// twenty seventeen breaks "entry-header" on large screen pages, commenting out for now
+				$string .= '<article id="post-'.get_the_ID().'" class="'.$post_class.'">';
+				//$string .= '<header class="entry-header">';
+				$string .= '<h2 class="entry-title"><a href="'.get_the_permalink().'">'.get_the_title().'</a></h2>';
+				$string .= '<div class="entry-meta">meta</div>';
+				//$string .= '</header>';
+				
+				// show the excerpt if show => 'excerpt', default is 'list' of titles
+				if ($new_atts['show'] == 'excerpt')
+				{
+					$string .= '<div class="entry-content">'.get_the_excerpt().'</div>';
+				}
+				
+				// show the full content if show => 'content', default is 'list' of titles
+				if ($new_atts['show'] == 'content')
+				{
+					$string .= '<div class="entry-content">'.get_the_content().'</div>';
+				}
+				
+				$string .= '</article>';
 			}
-			$string .= '</ul>';
 		}
 		
-		// reset to be nice, cuz we used a second loop
+		// reset to be nice, cuz we made a secondary loop
 		wp_reset_postdata();
 		
 		return $string;
+	}
+	
+	public function add_aj_shortcode_metabox()
+	{
+		/* If not in admin, then do nothing */
+		if (!is_admin())
+			return;
+		
+		/* Add the meta box */
+		add_meta_box(
+			'aj_shortcode_id',
+			'Shortcode',
+			array($this, 'build_shortcode_metabox'),
+			'astrojournal',
+			'side',
+			'default'
+		);
+	}
+	
+	public function build_shortcode_metabox($post)
+	{
+		// grab the post ID
+		$postID = $post->ID;
+		
+		// make it useful
+		echo '<p>To display just this post use:</p>';
+		echo '<p><em>[astrojournal p=' . $postID . ']</em></p>';
 	}
 	
 }
